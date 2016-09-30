c625fabc817291d6b9210db759981b23de365a05
tisong <tianxin675470193@outlook.com>
2016-09-30 21:25:31 +0800

    bug修改

--- programming_contest_challenge/get_start/datastruct/2_4_2/expedition.c
+++ programming_contest_challenge/get_start/datastruct/2_4_2/expedition.c
@@ -1,28 +1,43 @@
 /**
- *
  * 解题思路：经过了一个站，对于选择加油的权利是自由的
  *（可以选择加油，也可以选择不加油）
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 
 #define MAX_N 10000
+
+void inHeap(int);
+int  outHeap(void);
+
+
 int N = 0; // 加油站数量
 int L = 0; // 要行驶的长度
 int P = 0; // 初始化的汽油数(L)
+int A[MAX_N] = {0};
+int B[MAX_N] = {0};
 
 int heap[MAX_N + 1]; // 最大堆（0 元素并不使用）
 int size = 0;
 
 int solve() {
+	int sum = 0;
+	int x   = -1;
 
 	for (int i = 0; i < N;) {
 		if (P >= A[i]) {
-			// 入堆
+			inHeap(A[i++]);// 入堆
 		} else if (P < A[i]) {
-			P += ;// 出队
+			if ( (x = outHeap()) == -1) {
+				return -1;
+			}
+			P += x;
+			sum++;
 		}
 	}
+
+	return sum;
 }
 
 /* 入队 */
@@ -37,19 +52,45 @@ void inHeap(int x) {
 }
 
 /* 出队(返回最大值) */
-int outHeap() {
-	int i;
-	for (i = 1; i <= size;) {
-		if (heap[i*2] > heap[i*2+1]){
-			heap[i] = heap[i*2];
-			i = i*2;
-		} else {
-			heap[i] = heap[i*2+1];
-			i = i * 2 + 1;
-		}
+int outHeap(void) {
+	if (size == 0) {
+		return -1;
+	}
+
+	int i = 1;
+	while (i <= size) {
+		int x = heap[i*2] > heap[i*2+1] ? i*2 : i*2+1;
+		
+		heap[i] = heap[x];
+
+		i = x;
 	}
 	
 	size--;
 
 	return heap[i/2];
+}
+
+int main(void) {
+	printf("请输入加油站的数量 N =  ");
+	scanf("%d", &N);
+
+	printf("请输入行驶的距离 L = ");
+	scanf("%d", &L);
+
+	printf("请输入起始时汽油量(L) P = ");
+	scanf("%d", &P);
+
+	printf("请输入加油站的距离: ");
+	for (int i = 0; i < N; i++) {
+		scanf("%d", A + i);
+	}
+	printf("请输入加油站加油量: ");
+	for (int i = 0; i < N; i++) {
+		scanf("%d", B + i);
+	}
+
+	printf("最少加油次数: %d\n", solve());
+
+	return 0;
 }
