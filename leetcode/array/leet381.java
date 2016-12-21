2b7500da3a5d0794d2440820255ccb374ef4efa3
tisong <tianxin675470193@outlook.com>
2016-12-21 20:13:50 +0800

    实现插入 删除O(1)的数据结构

--- leetcode/array/leet381.java
+++ leetcode/array/leet381.java
@@ -10,8 +10,20 @@ public class leet381 {
 
 }
 
-// 如何实现插入 删除都在 O(1)的时间复杂度呢
-// 对于数组而言
+/**
+ * 如何实现插入 删除都在 O(1)的时间复杂度呢
+ * 对于数组而言，如何实现在O(1)时间复杂度的删除
+ * Map   : val - pos1, pos2, pos3...
+ * Array : pos1 - val, pos2 - val
+ * 插入:
+ *    1. 向数组末尾插入
+ *    2. 向Map中 val对应的有序Set集合中存放val在数组中的位置
+ * 删除:
+ *    1. 从Map中获取val对应有序Set集合中第1个pos
+ *    2. 数组中该pos要删除, 将pos位置上的元素替换为数组最末尾的元素
+ *    3. 删除Map中val对应有序Set集合中pos
+ */
+
 class RandomizedCollection {
     private List<Integer> arrays;
 
@@ -40,7 +52,7 @@ class RandomizedCollection {
         if (!res) return false;
 
         int pos = locs.get(val).iterator().next();
-
+        locs.get(val).remove(pos);
         if (pos < arrays.size() - 1) {
             // 如果不是最后一个元素
             int last = arrays.get(arrays.size()-1);
@@ -50,6 +62,7 @@ class RandomizedCollection {
         }
 
         arrays.remove(arrays.size()-1);
+
         if (locs.get(val).isEmpty()) locs.remove(val);
         return true;
     }
