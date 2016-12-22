4a213f69b64c539022d4d4e1e4412a2e27fb9696
tisong <tianxin675470193@outlook.com>
2016-12-22 22:42:28 +0800

    利用差值

--- leetcode/array/leet268.java
+++ leetcode/array/leet268.java
@@ -2,7 +2,14 @@
  * Created by tisong on 12/22/16.
  */
 public class leet268 {
-
+    public int missingNumber(int[] nums) {
+        int sum = nums.length * (nums.length + 1) / 2;
+        int sumOfNums = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sumOfNums += nums[i];
+        }
+        return sum - sumOfNums;
+    }
 }
 class Solution {
     // 错误的思路, 忘记考虑数组并不是有序的
